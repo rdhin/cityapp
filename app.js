a5e47426d1b245461ad8bd1e11ab742e3394f1a8
rdhin <41067402+rdhin@users.noreply.github.com>
2026-04-06 12:08:58 -0400

    no shinging

--- app.js
+++ app.js
@@ -7,12 +7,12 @@ app.get('/', (req, res) => res.redirect('/news'));
 
 // Journey 1: Get News Link
 app.get('/news', (req, res) => {
-    res.send('<h1>Daily News</h1><p>Today in NYC: Sun is shining.</p><a href="/news/article-1">Read More</a>');
+    res.send('<h1>Daily News</h1><p>Today in NYC: Sun is not shining.</p><a href="/news/article-1">Read More</a>');
 });
 
 // Journey 2: View Nearby Restaurants
 app.get('/restaurants', (req, res) => {
-    res.send('<h1>Nearby Restaurants</h1><ul><li>Pizza Palace - 1.3 miles</li><li>Sushi Central - 0.5 miles</li></ul>');
+    res.send('<h1>Nearby Restaurants</h1><ul><li>Pizza Palace - 1.3 miles< /li><li>Sushi Central - 0.5 miles</li></ul>');
 });
 
 app.listen(port, () => console.log(`Web App listening at http://localhost:${port}`));
